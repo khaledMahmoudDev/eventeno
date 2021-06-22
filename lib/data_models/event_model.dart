ffc35505f1c672307f409a2e691a90ab8e3c6387
khaled mahmoud <khaled.015067@ci.suez.edu.eg>
2021-06-22 22:49:01 +0200

    add event model add event viewmodels with list of events add events images

---
+++ lib/data_models/event_model.dart
@@ -0,0 +1,12 @@
+class Event {
+  String img;
+  String title;
+  int attended;
+  int subscribers;
+  List<String> date;
+  List<String> time;
+  List<String> categories;
+
+  Event(this.img, this.title, this.attended, this.subscribers, this.date,
+      this.time, this.categories);
+}
---
+++ lib/view_models/event_viewmodel.dart
@@ -0,0 +1,19 @@
+import 'package:eventeno/data_models/event_model.dart';
+
+final List<String> categories = ['Entry Ticket', 'Gate B1'];
+final List<String> time = ['04:30', '08:00', '10:15', '06:45'];
+final List<String> date = [
+  '22 May 2020',
+  '25 May 2020',
+  '26 May 2020',
+  '27 May 2020'
+];
+
+class EventsViewModel {
+  List<Event> events = [
+    Event('assets/images/eve1', 'Riyadh winter wonderland', 36542, 25461, date,
+        time, categories),
+    Event('assets/images/eve1', 'كرنفال الرعب Hunted House', 36542, 25461, date,
+        time, categories),
+  ];
+}
